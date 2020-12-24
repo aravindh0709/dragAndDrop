606d092712b04f2f7ffe08a0c99577e44a362ae1
Aravindhan <aravindhan@rently.com>
2020-12-24 23:27:55 +0530

    Drag and Drop circle component

--- App.js
+++ App.js
@@ -1,21 +1,85 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{ Component } from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  PanResponder,
+  Animated,
+  Dimensions
+} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class Viewport extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      showDraggable   : true,
+      dropZoneValues  : null,
+      pan             : new Animated.ValueXY()
+    };
+
+    this.panResponder = PanResponder.create({
+      onStartShouldSetPanResponder    : () => true,
+      onPanResponderMove              : Animated.event([null,{
+        dx  : this.state.pan.x,
+        dy  : this.state.pan.y
+      }]),
+      onPanResponderRelease           : () => {
+          Animated.spring(
+              this.state.pan,
+              {toValue:{x:0,y:0}}
+          ).start();
+      }
+    });
+  }
+
+  render(){
+    return (
+        <View style={styles.mainContainer}>
+          {this.renderDraggable()}
+        </View>
+    );
+  }
+
+  renderDraggable(){
+      return (
+          <View style={styles.draggableContainer}>
+            <Animated.View
+                {...this.panResponder.panHandlers}
+                style={[this.state.pan.getLayout(), styles.circle]}>
+              <Text style={styles.text}>Drag me!</Text>
+            </Animated.View>
+          </View>
+      );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+let CIRCLE_RADIUS = 36;
+let Window = Dimensions.get('window');
+let styles = StyleSheet.create({
+  mainContainer: {
+    flex    : 1
   },
+  dropZone    : {
+    height  : 100,
+    backgroundColor:'#2c3e50'
+  },
+  text        : {
+    marginTop   : 25,
+    marginLeft  : 5,
+    marginRight : 5,
+    textAlign   : 'center',
+    color       : '#fff'
+  },
+  draggableContainer: {
+    position    : 'absolute',
+    top         : Window.height/2 - CIRCLE_RADIUS,
+    left        : Window.width/2 - CIRCLE_RADIUS,
+  },
+  circle      : {
+    backgroundColor     : '#1abc9c',
+    width               : CIRCLE_RADIUS*2,
+    height              : CIRCLE_RADIUS*2,
+    borderRadius        : CIRCLE_RADIUS
+  }
 });
